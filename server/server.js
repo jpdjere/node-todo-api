0c7375449b7abb17e30230a012ede6391edbeb81
Juan Pablo Djeredjian <jpdjeredjian@Juans-MacBook-Pro.local>
2017-04-04 18:07:02 -0300

    Added GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const {ObjectID} = require('mongodb');
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -30,6 +32,24 @@ app.get('/todos', (req,res)=>{
   })
 })
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+
+})
 
 app.listen(3000, () => {
   console.log('Started server on port 3000')
