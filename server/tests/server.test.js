e3e01382847a0fea04b39de6f7d0b362c82bb6f7
Juan Pablo Djeredjian <jpdjeredjian@Juans-MacBook-Pro.local>
2017-05-14 16:28:20 -0300

    Added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -270,7 +270,7 @@ describe('POST /users', () => {
           //porque deberia estar hasheado
           expect(user.password).toNotBe(password);
           done();
-        })
+        }).catch((e) => done(e))
       });
   });
 
@@ -299,5 +299,68 @@ describe('POST /users', () => {
       .end(done);
   });
 
+})
+
+
+
+describe('POST /users/login', () => {
+
+  it('should login user and return auth token', (done) => {
+
+    supertest(app)
+      .post('/users/login')
+      .send({
+        email:users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      //Check if x-auth header is sent back as token
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        //Check if the user returned has an auth access and the token
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access:'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e))
+      })
+
+  });
+
+  it('should reject invalid login', (done) => {
+
+    supertest(app)
+      .post('/users/login')
+      .send({
+        email:users[1].email,
+        //Pass in an invalid password
+        password: users[1].email + '123randomshit'
+      })
+      .expect(400)
+      //Check if x-auth header is sent back as token
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        //Check if the user returned has an auth access and the token
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e))
+      })
+
+  });
 
 })
